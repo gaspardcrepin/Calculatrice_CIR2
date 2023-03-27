d9c3f9548886024bfbca91f783d56c7c47e350ba
Gaspard CREPIN <gaspard.crepin@student.junia.com>
2023-03-27 18:55:40 +0200

    Partie 3

--- calc.js
+++ calc.js
@@ -1,28 +1,36 @@
 console.log("Gaspard's log test")
 
-function printNumber(number){
-    console.log(number);
-    let element = document.getElementById("span-calcul");
-    element.textContent=element.textContent+number;
+class BaseCalculator {
+    constructor() {
+        
+    }
 
-    let element_above = document.getElementById("left-part");
-    element_above.textContent=element_above.textContent+number;
+    printNumber(number){
+        console.log(number);
+        let element = document.getElementById("span-calcul");
+        element.textContent=element.textContent+number;
+    
+        let element_above = document.getElementById("left-part");
+        element_above.textContent=element_above.textContent+number;
+    }
+    
+    result(){
+        let element = document.getElementById("span-calcul");
+        let result = eval(element.textContent);
+    
+        let element_affichage = document.getElementById("result");
+        element_affichage.textContent = result
+        console.log(result);
+    }
+    
+    erase(){
+        console.log("erase")
+        let element = document.getElementById("span-calcul");
+        element.textContent=element.textContent.slice(0, element.textContent.length - 1);
+    
+        let element_above = document.getElementById("left-part");
+        element_above.textContent=element_above.textContent.slice(0, element_above.textContent.length - 1);
+    }
 }
 
-function result(){
-    let element = document.getElementById("span-calcul");
-    let result = eval(element.textContent);
-
-    let element_affichage = document.getElementById("result");
-    element_affichage.textContent = result
-    console.log(result);
-}
-
-function erase(){
-    console.log("erase")
-    let element = document.getElementById("span-calcul");
-    element.textContent=element.textContent.slice(0, element.textContent.length - 1);
-
-    let element_above = document.getElementById("left-part");
-    element_above.textContent=element_above.textContent.slice(0, element_above.textContent.length - 1);
-}
+let baseCalculator = new BaseCalculator();
